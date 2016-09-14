ef2265860af844a03c2274fd918fc364e77c85a8
Jeppe Pihl <jpihl08@gmail.com>
2016-09-14 09:42:04 +0200

    break windows?

--- test/src/extractor/test_timestamp_extractor_layer.cpp
+++ test/src/extractor/test_timestamp_extractor_layer.cpp
@@ -50,69 +50,69 @@ TEST(extractor_test_timestamp_extractor_layer, api)
     EXPECT_FALSE(stack.open());
     EXPECT_EQ(1U, layer.close.calls());
 
-    // dummy_trak trak;
+    dummy_trak trak;
 
-    // //-------------//
-    // // create stts //
-    // //-------------//
+    //-------------//
+    // create stts //
+    //-------------//
 
-    // // This buffer is made up, to create a dummy stts box. please see
-    // // stts.hpp for information related to this.
-    // std::vector<uint8_t> stts_buffer =
-    // {
-    //     // These values have already been read by the parser:
-    //     // 0x00, 0x00, 0x00, 0xXX, // box size
-    //     // 's', 't', 't', 's', // box type
-    //     0x00, // full_box version
-    //     0x00, 0x00, 0x00, // full_box flag
-    //     0x00, 0x00, 0x00, 0x01, // stts entry count 1
-    //     0x00, 0x00, 0x00, 0x03, // stts entry sample_count
-    //     0x00, 0x00, 0x00, 0x06, // stts entry sample_delta
-    // };
-    // // size including attributes read by parser:
-    // auto stts_size = stts_buffer.size() + 8;
+    // This buffer is made up, to create a dummy stts box. please see
+    // stts.hpp for information related to this.
+    std::vector<uint8_t> stts_buffer =
+    {
+        // These values have already been read by the parser:
+        // 0x00, 0x00, 0x00, 0xXX, // box size
+        // 's', 't', 't', 's', // box type
+        0x00, // full_box version
+        0x00, 0x00, 0x00, // full_box flag
+        0x00, 0x00, 0x00, 0x01, // stts entry count 1
+        0x00, 0x00, 0x00, 0x03, // stts entry sample_count
+        0x00, 0x00, 0x00, 0x06, // stts entry sample_delta
+    };
+    // size including attributes read by parser:
+    auto stts_size = stts_buffer.size() + 8;
 
-    // petro::byte_stream stts_byte_stream(stts_buffer.data(), stts_buffer.size());
-    // auto stts =
-    //     std::make_shared<petro::box::stts>(std::weak_ptr<petro::box::box>());
-    // stts->read(stts_size, stts_byte_stream);
+    petro::byte_stream stts_byte_stream(stts_buffer.data(), stts_buffer.size());
+    auto stts =
+        std::make_shared<petro::box::stts>(std::weak_ptr<petro::box::box>());
+    stts->read(stts_size, stts_byte_stream);
 
-    // trak.m_stts = stts;
+    trak.m_stts = stts;
 
-    // //-------------//
-    // // create mdhd //
-    // //-------------//
+    //-------------//
+    // create mdhd //
+    //-------------//
 
-    // // This buffer is made up, to create a dummy mdhd box. please see
-    // // mdhd.hpp for information related to this.
-    // std::vector<uint8_t> mdhd_buffer =
-    // {
-    //     // These values have already been read by the parser:
-    //     // 0x00, 0x00, 0x00, 0xXX, // box size
-    //     // 'm', 'd', 'h', 'd', // box type
-    //     0x00, // full_box version
-    //     0x00, 0x00, 0x00, // full_box flag
-    //     0x00, 0x00, 0x00, 0x00, // mdhd m_creation_time
-    //     0x00, 0x00, 0x00, 0x00, // mdhd m_modification_time
-    //     0x00, 0x00, 0x00, 0x42, // mdhd m_timescale
-    //     0x00, 0x00, 0x00, 0x00, // mdhd m_duration
-    //     0x00, 0x00, // mdhd langauge code
-    //     0x00, 0x00  // mdhd predefined
-    // };
-    // // size including attributes read by parser:
-    // auto mdhd_size = mdhd_buffer.size() + 8;
+    // This buffer is made up, to create a dummy mdhd box. please see
+    // mdhd.hpp for information related to this.
+    std::vector<uint8_t> mdhd_buffer =
+    {
+        // These values have already been read by the parser:
+        // 0x00, 0x00, 0x00, 0xXX, // box size
+        // 'm', 'd', 'h', 'd', // box type
+        0x00, // full_box version
+        0x00, 0x00, 0x00, // full_box flag
+        0x00, 0x00, 0x00, 0x00, // mdhd m_creation_time
+        0x00, 0x00, 0x00, 0x00, // mdhd m_modification_time
+        0x00, 0x00, 0x00, 0x42, // mdhd m_timescale
+        0x00, 0x00, 0x00, 0x00, // mdhd m_duration
+        0x00, 0x00, // mdhd langauge code
+        0x00, 0x00  // mdhd predefined
+    };
+    // size including attributes read by parser:
+    auto mdhd_size = mdhd_buffer.size() + 8;
 
-    // petro::byte_stream mdhd_byte_stream(mdhd_buffer.data(), mdhd_buffer.size());
-    // auto mdhd =
-    //     std::make_shared<petro::box::mdhd>(std::weak_ptr<petro::box::box>());
-    // mdhd->read(mdhd_size, mdhd_byte_stream);
+    petro::byte_stream mdhd_byte_stream(mdhd_buffer.data(), mdhd_buffer.size());
+    auto mdhd =
+        std::make_shared<petro::box::mdhd>(std::weak_ptr<petro::box::box>());
+    mdhd->read(mdhd_size, mdhd_byte_stream);
 
-    // trak.m_mdhd = mdhd;
+    trak.m_mdhd = mdhd;
 
-    // layer.trak.set_return(&trak);
-    // layer.sample_index.set_return(0);
+    layer.trak.set_return(&trak);
+    layer.sample_index.set_return(0);
 
-    // // test stack
+    // test stack
 
     // EXPECT_TRUE(stack.open());
 
