512ce7fc571d8ef4b605a0cf58d9682ba33db365
Jeppe Pihl <jpihl08@gmail.com>
2017-07-21 18:42:50 +0200

    move avc_sample_access to a layer

--- test/src/extractor/test_annex_b_writer_layer.cpp
+++ test/src/extractor/test_annex_b_writer_layer.cpp
@@ -12,16 +12,13 @@ namespace
 {
 struct dummy_layer
 {
-    stub::function<bool()> open;
-    stub::function<void()> close;
-    stub::function<void()> reset;
-    stub::function<void()> advance;
     stub::function<void(uint8_t*)> write_nalu_header;
     stub::function<uint32_t()> nalu_header_size;
-    stub::function<bool()> at_end;
-    stub::function<uint32_t()> nalu_length_size;
-    stub::function<const uint8_t* ()> sample_data;
-    stub::function<uint32_t()> sample_size;
+
+    stub::function<uint32_t()> nalu_count;
+    stub::function<const uint8_t*(uint32_t)> nalu_at;
+    stub::function<uint32_t(uint32_t)> nalu_size_at;
+
 };
 
 using dummy_stack = petro::extractor::annex_b_writer_layer<dummy_layer>;
@@ -31,46 +28,30 @@ TEST(extractor_annex_b_writer_layer, api)
 {
     dummy_stack stack;
     dummy_layer& layer = stack;
-    layer.open.set_return(false);
-    EXPECT_FALSE(stack.open());
-    EXPECT_EQ(1U, layer.close.calls());
 
-    std::vector<uint8_t> avc_sample
-    {
-        0x00, 0x00, 0x03, 0x01, 0x02, 0x03,
-        0x00, 0x00, 0x04, 0x01, 0x02, 0x03, 0x04,
-        0x00, 0x00, 0x02, 0x01, 0x02
-    };
-
-    layer.at_end.set_return(false);
-    layer.nalu_length_size.set_return(3U);
-    layer.sample_data.set_return(avc_sample.data());
-    layer.sample_size.set_return(avc_sample.size());
-
+    layer.nalu_count.set_return(2U);
     layer.nalu_header_size.set_return(4U);
+    layer.nalu_size_at.set_return(3U);
 
-    layer.open.set_return(true);
-    EXPECT_TRUE(stack.open());
-    auto expected_annex_b_size = 9U + 3U * 4U;
+    auto expected_annex_b_size = 2U * (4U + 3U);
 
     EXPECT_EQ(expected_annex_b_size, stack.annex_b_size());
 
     std::vector<uint8_t> expected_annex_b =
         {
             0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03,
-            0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04,
-            0x00, 0x00, 0x00, 0x00, 0x01, 0x02
+            0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03,
         };
 
+    std::vector<uint8_t> nalu = {0x01, 0x02, 0x03};
+    layer.nalu_at.set_return(nalu.data());
+
     std::vector<uint8_t> annex_b(stack.annex_b_size());
+    ASSERT_EQ(expected_annex_b.size(), stack.annex_b_size());
     stack.write_annex_b(annex_b.data());
     EXPECT_EQ(expected_annex_b, annex_b);
     EXPECT_TRUE(layer.write_nalu_header.expect_calls()
                 .with(annex_b.data())
                 .with(annex_b.data() + 7)
-                .with(annex_b.data() + 7 + 8)
                 .to_bool());
-
-    layer.at_end.set_return(true);
-    stack.advance();
 }
