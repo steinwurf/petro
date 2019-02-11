88b70e9ff65efdbdc67db11d00c819ef9ccee1d6
Jeppe Pihl <jpihl08@gmail.com>
2019-02-11 15:35:05 +0100

    fix warning

--- test/src/extractor/test_avc_to_annex_b.cpp
+++ test/src/extractor/test_avc_to_annex_b.cpp
@@ -34,11 +34,9 @@ TEST(extractor_test_avc_to_annex_b, init)
         test_sample.data(), test_sample.size(), nalu_size_length);
 
     EXPECT_EQ(5U, samples.size());
-    uint64_t sum_size = (uint64_t)std::accumulate(
-        samples.begin(),
-        samples.end(),
-        0U,
-        [](uint64_t accumlate, auto s){ return accumlate + s.second; });
+    uint64_t sum_size = 0;
+    for (auto s : samples)
+        sum_size += s.second;
     (void) sum_size;
     // uint64_t expected_sum_size = sample_size - (samples.size() * nalu_size_length);
     // EXPECT_EQ(expected_sum_size, sum_size);
